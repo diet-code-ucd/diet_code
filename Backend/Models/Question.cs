7a74236664fa5068b17560e9731cea405d34b0d2
Ross Murphy <ross.murphy@blacktower.xyz>
2023-06-14 22:33:27 +0100

    Add basic postgres db and configure the connection in the backend

--- Backend/Models/Question.cs
+++ Backend/Models/Question.cs
@@ -1,13 +1,10 @@
-using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
 
 namespace Backend.Models;
 
 public class Question {
     public int Id { get; set;}
-    public string? question { get; set; }
-    public string? optionA { get; set; }
-    public string? optionB { get; set; }
-    public string? optionC { get; set; }
-    public string? optionD { get; set; }
-    public string? answer { get; set; }
+    public string Query { get; set;}
+    public HashSet<AnswerOption> Options { get; set; }
+    public int Answer { get; set; }
 }
