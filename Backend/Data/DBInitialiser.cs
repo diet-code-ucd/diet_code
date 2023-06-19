4977cf25500b2ceba1c46fabc3d4d8a9bfa24044
Ross Murphy <ross.murphy@blacktower.xyz>
2023-06-19 11:05:58 +0100

    Model changes - start from the bottom

--- Backend/Data/DBInitialiser.cs
+++ Backend/Data/DBInitialiser.cs
@@ -6,8 +6,18 @@ public static class DbInitialiser {
     public static void Initialise(DatabaseContext context) {
         context.Database.EnsureCreated();
 
+        Course math101 = new Course {Name = "Math 101"};
+        Course math102 = new Course {Name = "Math 102"};
+        Course math103 = new Course {Name = "Math 103"};
+
+        List<Course> courses = new List<Course> {
+            math101, math102, math103
+        };
+
+        context.AddRange(courses);
+
         List<User> users = new List<User> {
-            new User {Username = "user1", Password = "pw1"},
+            new User {Username = "user1", Password = "pw1", EnrolledCourses = new List<Course> { math101 } },
             new User {Username = "user2", Password = "pw2"},
             new User {Username = "user3", Password = "pw3"},
         };
--- Backend/Models/User.cs
+++ Backend/Models/User.cs
@@ -4,16 +4,19 @@ public class User {
      public int Id { get; set;}
      public string Username { get; set;}
      public string Password { get; set;}
+     public ICollection<Course> EnrolledCourses { get; set;}
 }
 
 public class UserDTO {
      public int Id { get; set;}
      public string Username { get; set;}
+     public ICollection<Course>? EnrolledCourses { get; set;}
 
      public static UserDTO Convert(User user) {
         return new UserDTO { 
             Id = user.Id,
-            Username = user.Username
+            Username = user.Username,
+            EnrolledCourses = user.EnrolledCourses,
         };
      }
 }
