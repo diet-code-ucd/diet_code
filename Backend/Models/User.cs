f47cfde8e042797b7fc654f60848f5b8b2cfcc75
KartikDhongadi <kartik.dhongadi16@gmail.com>
2023-06-24 03:27:33 +0100

    Updated User.cs to fetch username password from the Users table
    Need to work on encryption and how to put data into DB from login (right now just using test username and password)

--- Backend/Models/User.cs
+++ Backend/Models/User.cs
@@ -1,11 +1,54 @@
+using System;
 using Microsoft.EntityFrameworkCore;
 
 namespace Backend.Models;
 
 public class User {
-     public int Id { get; set;}
+     //public int Id { get; set;}
      public string Username { get; set;}
-     public string Password { get; set;}
-     public virtual ICollection<Course>? EnrolledCourses { get; set;}
-     public virtual ICollection<Test>? Tests { get; set;}
+     public string pwd { get; set;}
+     //public virtual ICollection<Course>? EnrolledCourses { get; set;}
+     //public virtual ICollection<Test>? Tests { get; set;}
+}
+
+public class AppDbContext : DbContext
+{
+    public DbSet<User> Users { get; set; }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>().HasNoKey();
+    }
+
+    public static bool ValidateUserCredentials(string username, string password)
+    {
+        var options = new DbContextOptionsBuilder<AppDbContext>()
+            .UseSqlServer("your-connection-string")
+            .Options;
+
+        using (var dbContext = new AppDbContext(options))
+        {
+            var user = dbContext.Users.FirstOrDefault(u => u.Username == username && u.pwd == password);
+            return user != null;
+        }
+    }
+
+    //To check if the password and username is correct (Need to work on encryption)
+    //bool isValid = ValidateUserCredentials(username, password);
+
+    //    if (isValid)
+    //    {
+    //        Console.WriteLine("Login successful!");
+    //    }
+    //    else
+    //    {
+    //        Console.WriteLine("Invalid username or password.");
+    //    }
+
+
+
 }
