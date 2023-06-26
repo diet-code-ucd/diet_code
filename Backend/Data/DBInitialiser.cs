fe3e17c3e54afefaf82c103957ab1f0e18963cd4
Ross Murphy <ross.murphy@blacktower.xyz>
2023-06-26 15:10:42 +0100

    Basic database setup

--- Backend/Data/DBInitialiser.cs
+++ Backend/Data/DBInitialiser.cs
@@ -1,3 +1,5 @@
+using System.Drawing.Text;
+using System.Text.Json;
 using Backend.Models;
 
 namespace Backend.Data;
@@ -15,37 +17,33 @@ public static class DbInitialiser
             return;
         }
 
-        // divide the answersList into 4 answer options for each newly created question
-        List<Question> questionsList = new List<Question>();
-        for (int i = 0; i < 12; i++)
-        {
-            Question question = new Question { 
-                QuestionText = $"Question {i + 1}",
-                Answer = $"Answer {i + 1}",
-                Explanation = $"Explanation {i + 1}",
-                Difficulty = Difficulty.Easy
-            };
-            questionsList.Add(question);
-        }
+        // Read the json file containing the questions Data/combined_df.json
+        string jsonString = File.ReadAllText("Data/combined_df.json");
+        Console.WriteLine(jsonString);
+        // wait for user input
+        Console.ReadLine();
 
-        context.AddRange(questionsList);
+        // Convert the json string to a list of Question objects
+        List<Question> questions = JsonSerializer.Deserialize<List<Question>>(jsonString)!;
+        Console.WriteLine(questions.Count);
+        Console.WriteLine(questions.First().QuestionText);
+        Console.ReadLine();
+        context.AddRange(questions);
 
-        Test test1 = new Test { Questions = questionsList.GetRange(0, 5) };
+        Test test1 = new Test { Questions = questions.GetRange(0, 5) };
 
         context.Add(test1);
 
-        Course math101 = new Course { Name = "Math 101", Questions = questionsList };
-        Course math102 = new Course { Name = "Math 102" };
-        Course math103 = new Course { Name = "Math 103" };
+        Course math = new Course { Name = "Math", Questions = questions };
 
         List<Course> courses = new List<Course> {
-            math101, math102, math103
+            math
         };
 
         context.AddRange(courses);
 
         List<User> users = new List<User> {
-            new User {Username = "user1", Password = "pw1", EnrolledCourses = new HashSet<Course> { math101 }, Tests = new List<Test> { test1 } },
+            new User {Username = "user1", Password = "pw1", EnrolledCourses = new HashSet<Course> { math }, Tests = new List<Test> { test1 } },
             new User {Username = "user2", Password = "pw2"},
             new User {Username = "user3", Password = "pw3"},
         };
