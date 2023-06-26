0aa76eb1db461bcc3a3556424ec7b3bf75ac0af0
Ross Murphy <ross.murphy@blacktower.xyz>
2023-06-26 01:11:46 +0100

    Add allow any header to cors

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -12,7 +12,8 @@ builder.Services.AddCors(options =>
     options.AddDefaultPolicy(
         policy =>
         {
-            policy.WithOrigins("http://localhost:5173");
+            policy.WithOrigins("http://localhost:5173")
+            .AllowAnyHeader();
         });
 });
 
