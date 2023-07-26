24b420803cf62e9b7e478b380ca327b58d8dacc6
rmurphy <ross.murphy@blacktower.xyz>
2023-07-26 18:33:15 +0100

    Model changes (#32)
    * Some basic model changes and some seed data
    
    * Make some changes to the model and add a jupyter notbook for scripts
    
    ---------
    
    Co-authored-by: Ross Murphy <rm@grianainech-dev.grianainech>

--- its-be/be/models/__init__.py
+++ its-be/be/models/__init__.py
@@ -1,4 +1,4 @@
 # /models/__init__.py
 from .database import db
 from .user import User
-from .tutor import Course, Test, Question
+from .tutor import Course, Test, Question, Option, Tag, UserAnswer
--- its-be/be/models/tutor.py
+++ its-be/be/models/tutor.py
@@ -1,16 +1,39 @@
-from pony.orm import Required, Set
+from pony.orm import Required, Set, Optional
 from .database import db
 from .user import User
 
 class Course(db.Entity):
+    name = Required(str)
     test = Set('Test')
+    questions = Set('Question')
     enrolled_users = Set(User)
 
 class Test(db.Entity):
     course = Required(Course)
     questions = Set('Question')
+    user_answers = Set('UserAnswer')
     for_user = Set(User)
 
 class Question(db.Entity):
+    course = Required(Course)
     tests = Set(Test)
+    question = Required(str)
+    answer = Required(str)
+    options = Set('Option')
+    difficulty = Required(str)
+    tags = Set('Tag')
+    user_answers = Set('UserAnswer')
+
+class Option(db.Entity):
+    question = Required(Question)
+    option = Required(str)
+
+class Tag(db.Entity):
+    question = Set(Question)
+    tag = Required(str, unique=True)
+
+class UserAnswer(db.Entity):
+    test = Required(Test)
+    question = Required(Question)
+    answer = Required(str)
 
