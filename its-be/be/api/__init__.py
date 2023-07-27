2d4651e88e15d02bb39bf7adf8c7122cf8f9f39c
Ross Murphy <ross.murphy@blacktower.xyz>
2023-07-27 22:42:47 +0100

    Remove test api to fix circular dependency

--- its-be/be/api/__init__.py
+++ its-be/be/api/__init__.py
@@ -1,10 +1,10 @@
 from flask import Blueprint
 
-from . import course, test
+from . import course
 
 bp = Blueprint('api', __name__, url_prefix='/api')
 bp.register_blueprint(course.bp)
-bp.register_blueprint(test.bp)
+# bp.register_blueprint(test.bp)
 
 
 
