1939ed08c661110a62da34e078bf67bf26ff587d
Ross Murphy <ross.murphy@blacktower.xyz>
2023-07-31 14:11:27 +0100

    Fix various issue with launching app

--- its-be/be/ml.py
+++ its-be/be/ml.py
@@ -1,6 +1,5 @@
 from langchain.llms import OpenAI
-from langchain.chat_models import ChatOpenAI
-from langchain.prompts.chat import (
+from langchain.prompts import (
     ChatPromptTemplate,
     SystemMessagePromptTemplate,
     HumanMessagePromptTemplate,
