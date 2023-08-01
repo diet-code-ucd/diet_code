5749f2a7dbef76e344a0ce45fd319fa326dbead3
Jayesh <jayesh.bhatt@ucdconnect.ie>
2023-08-01 13:30:06 +0100

    more stuff

--- its-be/be/views.py
+++ its-be/be/views.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, abort, render_template, request
+import json
+from flask import Blueprint, abort, jsonify, render_template, request
 from flask_login import login_required, current_user
 from pony.flask import db_session
 
@@ -27,10 +28,38 @@ def course_details(course_id):
 @login_required
 def userTest():
     course_id=request.args.get('course_id')
+    course = Course.get(id = course_id)
     test = Test.get(id=1)
-    return render_template("user_test.html", user=current_user, course_id=course_id, test=test)
+    return render_template("user_test.html", user=current_user, course_id=course_id, test=test, course = course)
 
 @views.route('/userstats', methods=['GET', 'POST'])
 @login_required
 def userStats():
     return render_template("user_stats.html")
+
+@views.route('/submit_test', methods=['GET', 'POST'])
+@login_required
+def submit_test():
+    if request.method == 'POST':
+        # Get the JSON data from the URL parameter
+        data = request.args.get('data')
+        data = json.loads(data)  # Convert the data to a Python dictionary
+
+        # Process the data as needed
+        questions = data.get('questions', [])
+        for question in questions:
+            question_id = question.get('id')
+            user_answer = question.get('user_answer')
+            # Do something with the question_id and user_answer
+
+        # After processing the data, we can return a response if needed
+        return jsonify({'message': 'Data processed successfully'})
+
+    else:
+        # If the method is GET, you can render the "submit_test.html" template
+        return render_template('submit_test.html')
+    
+@views.route('/aboutus', methods=['GET', 'POST'])
+@login_required
+def about_us():
+    return render_template("about_us.html")
