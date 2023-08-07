edbb9b6995cf0071bdb73ec8d1ac88d5be228277
Ross Murphy <ross.murphy@blacktower.xyz>
2023-08-07 02:17:23 +0100

    add previous tests, needs ui

--- its-be/be/views.py
+++ its-be/be/views.py
@@ -37,11 +37,18 @@ def course_details(course_id):
     if not course:
         abort(404)
     topics = []
+    completed_tests = []
+    incomplete_tests = []
     if user_is_enrolled:
         usc = UserCourseSelection[current_user, course]
         topics = usc.topics
+        for test in usc.tests.filter(lambda t: t.completed):
+            completed_tests.append(test)
+        for test in usc.tests.filter(lambda t: not t.completed):
+            incomplete_tests.append(test)
+        print(completed_tests)
     available_topics = [topic for topic in course.topics if topic not in topics]
-    return render_template("course_details.html", course = course.to_dict(), enrolled=user_is_enrolled, topics=topics, available_topics=available_topics)
+    return render_template("course_details.html", course = course.to_dict(), enrolled=user_is_enrolled, topics=topics, available_topics=available_topics, tests_comp=completed_tests, tests_incomp=incomplete_tests)
 
 @views.route('/userTest', methods=['GET', 'POST'])
 @login_required
